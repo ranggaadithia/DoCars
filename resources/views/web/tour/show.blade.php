a7e6f3cb020b2140e6e87e1ba456fed170f81b87
Rangga Adithia <rangga.adithia26@gmail.com>
2024-01-11 12:39:29 +0800

    slicing: package

--- resources/views/web/tour/show.blade.php
+++ resources/views/web/tour/show.blade.php
@@ -5,22 +5,22 @@
   @include('components.navbar')
 
     <div class="d-show h-100 bg-white" style="overflow: hidden">
-      <img src="{{ asset('storage/'. $package->image) }}" alt="" class="img-fluid w-100" style="background-size: cover">
+      <img src="{{ asset('storage/'. $package->image) }}" alt="" class="img-fluid w-100 h-100" style="object-fit: cover">
       <h1 class="display-4" data-aos="fade-up" data-aos-delay="300" data-aos-duration="800" style="z-index: 1000; position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%)">{{ $package->name }}</h1>
       <div class="line-patern mx-auto" data-aos="fade-up" data-aos-delay="300" data-aos-duration="800" style="z-index: 1000; position: absolute; top: 60%; left: 50%; transform: translate(-50%, -50%)"></div>
     </div>
 
 
   <div class="container mb-5">
-    <h1 class="text-center mb-5 pt-3" data-aos="fade-up">Tours Destination</h1>
+    <h1 class="text-center mb-5 pt-3" data-aos="fade-up">Itinerary</h1>
 
     @foreach ($destinations as $destination)
-      <div class="row justify-content-center">
-        <div class="col-lg-6 mt-5-desktop">
+      <div class="row justify-content-center mt-5">
+        <div class="col-lg-6">
           <h3 class="pb-2" style="font-weight: 700;">{{ $destination->name }} </h3>
           <p class="pb-2" style="line-height: 25px;">{{ $destination->description }}</p>
         </div>
-        <div class="col-lg-6 d-desktop">
+        <div class="col-lg-6">
           <img src="{{ asset('storage/'. $destination->photos->first()->image) }}" class="figure-img img-fluid rounded" alt="Klingking Beach">
         </div>
       </div>
